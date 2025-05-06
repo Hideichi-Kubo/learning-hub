77f9887a63ed8488134c2626f31154279de2b7a7
Hideichi Kubo <unthought_known@outlook.jp>
2025-05-07 05:20:21 +0900

    画像詳細情報HTTP通信インターフェース作成 - DTOの作成 - Retrofit関数の追加 - PhotoRipositoryに関数追加

--- UnsplashClient/app/src/main/java/com/example/unsplashclient/data/repository/DefaultPhotoRepository.kt
+++ UnsplashClient/app/src/main/java/com/example/unsplashclient/data/repository/DefaultPhotoRepository.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashclient.data.repository
 
+import com.example.unsplashclient.data.remote.PhotoDetailDto
 import com.example.unsplashclient.data.remote.SearchPhotosResultDto
 import com.example.unsplashclient.data.remote.UnsplashApiService
 import com.example.unsplashclient.domain.repository.PhotoRepository
@@ -10,5 +11,9 @@ class DefaultPhotoRepository @Inject constructor(
 ) : PhotoRepository {
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return apiService.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(id: String): PhotoDetailDto {
+        return apiService.getPhotoById(id)
     }
 }
