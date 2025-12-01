c0e2c0b3b29ff815a8c9d0c37b7967fffc58e99a
Hideichi Kubo <unthought_known@outlook.jp>
2025-12-01 17:51:02 +0900

    画面検索画面の基盤実装 - SearchPhotosStateの作成 - SearchPhotosViewModelの作成 - 画像検索画面のViewのベースを作成 - Hiltのセットアップ

--- UnsplashClient/app/src/main/java/com/example/unsplashclient/common/Constants.kt
+++ UnsplashClient/app/src/main/java/com/example/unsplashclient/common/Constants.kt
@@ -2,4 +2,5 @@ package com.example.unsplashclient.common
 
 object Constants {
     const val API_KEY = "TxK7RLi8HKz-AF_HgZwBwKDOE7tlcUt441BHZXY1Qas"
+    const val BASE_URL = "https://api.unsplash.com"
 }
--- UnsplashClient/app/src/main/java/com/example/unsplashclient/presentation/MainActivity.kt
+++ UnsplashClient/app/src/main/java/com/example/unsplashclient/presentation/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.unsplashclient.presentation.search_photos.SearchPhotosScreen
 import com.example.unsplashclient.presentation.ui.theme.UnsplashClientTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     ) {
                         composable(route = ScreenRoute.SearchScreen.route) {
-                            // TODO
+                            SearchPhotosScreen()
                         }
                         composable(route = ScreenRoute.PhotoDetailScreen.route) {
                             // TODO
