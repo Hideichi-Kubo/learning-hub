d1d679fd81a1f0c6f9f16ce321f72adea7ef9650
Hideichi Kubo <unthought_known@outlook.jp>
2026-01-01 14:31:11 +0900

    コミット忘れをコミット

--- UnsplashClient/app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailState.kt
+++ UnsplashClient/app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailState.kt
@@ -1,3 +1,9 @@
 package com.example.unsplashclient.presentation.photo_detail
 
-data class PhotoDetailState()
+import com.example.unsplashclient.domain.model.PhotoDetail
+
+data class PhotoDetailState(
+    val isLoading: Boolean = false,
+    val photoDetail: PhotoDetail? = null,
+    val error: String? = null
+)
